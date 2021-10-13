aac87ac8c849065ba1c4b55d294f4c574fe6e2e6
User <User@LAB2-12>
2021-10-13 14:04:07 +0300

    sync

--- DealAndTripChennyShahirEndProjectServer/Controllers/DealAndTripProjectServerController.cs
+++ DealAndTripChennyShahirEndProjectServer/Controllers/DealAndTripProjectServerController.cs
@@ -4,12 +4,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using DealAndTripChennyShahirEndProjectServerBL.Models;
+using System.IO;
 namespace DealAndTripChennyShahirEndProjectServer.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class DealAndTripProjectServerController : ControllerBase
     {
+        #region Add connection to the db context using dependency injection
+        DealAndTripDBContext context;
+        public DealAndTripProjectServerController(DealAndTripDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
     }
 }
